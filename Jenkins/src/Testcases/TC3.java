29c0d6055d5a651c0eab7cb435b0d15cf44ec523
Alka <Alka@192.168.2.12>
2017-08-11 12:08:36 -0400

    automation

---
+++ Jenkins/src/Testcases/TC3.java
@@ -0,0 +1,21 @@
+package Testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC3 {
+	@Test
+	public void tastcase()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alka\\workspace2\\Jenkins\\drivers\\chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://facebook.com");
+	driver.findElement(By.id("email")).sendKeys("Hello");
+	driver.findElement(By.id("pass")).sendKeys("Hello");
+	driver.quit();
+	
+	
+	}
+
+}
